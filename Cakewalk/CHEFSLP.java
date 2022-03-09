f1b3057bd67e7e8e3c8fc169b366d3d0fe8aaad4
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-09 09:19:37 +0530

    Chef and Pairing Slippers

---
+++ Cakewalk/CHEFSLP.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t, n, x, l, temp;
+        t = sc.nextInt();
+        while (t > 0) {
+            n = sc.nextInt();
+            l = sc.nextInt();
+            x = sc.nextInt();
+            temp = n - l;
+            temp = Math.min(temp, l);
+            System.out.println(temp * x);
+            t -= 1;
+        }
+    }
+}
