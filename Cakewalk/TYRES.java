b45bfb82fda0aa29196d9fa463e7f07958270fc4
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-09 09:08:11 +0530

    Cars and Bikes

---
+++ Cakewalk/TYRES.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner s = new Scanner(System.in);
+        int t, n;
+        t = s.nextInt();
+        while (t > 0) {
+            n = s.nextInt();
+            if (n >= 4) {
+                n = n % 4;
+            }
+            if (n >= 2) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            t -= 1;
+        }
+    }
+}
