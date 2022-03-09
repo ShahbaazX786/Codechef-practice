6e59c884a46c7518e51c330b6d01e8c97365f35d
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-09 09:17:03 +0530

    Janmansh and Coins

---
+++ Cakewalk/JCOINS.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t, x, y;
+        t = sc.nextInt();
+        while (t > 0) {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            x *= 10;
+            y *= 5;
+            System.out.println(x + y);
+            t -= 1;
+        }
+    }
+}
