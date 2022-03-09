828a777d56a6079557f0cf31f3b07c4f61c924de
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-09 13:44:37 +0530

    Chef and His Fruit Stand

---
+++ Cakewalk/FRUITCHAAT.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t, a[] = new int[2];
+        t = sc.nextInt();
+        while (t > 0) {
+            for (int i = 0; i < 2; i++) {
+                a[i] = sc.nextInt();
+            }
+            a[0] /= 2;
+            // System.out.println(a[0]+" "+a[1]);
+            System.out.println(a[0] < a[1] ? a[0] : a[1]);
+            t -= 1;
+        }
+    }
+}
