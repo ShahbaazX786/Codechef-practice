cd637ddf0a20f1c7d7216d43e644298115f7afa7
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-09 13:39:13 +0530

    Watching Movies at 2x

---
+++ Cakewalk/MOVIE2X.java
@@ -0,0 +1,18 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        y /= 2;
+        System.out.println(x - y);
+    }
+}
