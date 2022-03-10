c85f9149cc295f0415da3d7bc6576de9c43d378a
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-10 11:01:02 +0530

    Two vs Ten

---
+++ Cakewalk/TWOVSTEN.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            int temp = 0;
+            int x = sc.nextInt();
+            if (x % 10 == 0) {
+                System.out.println(0);
+            } else if (x % 10 == 5) {
+                System.out.println(1);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
