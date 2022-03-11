5aa27c9e82a2900b5724feb16707c233ae84c730
Shahbaaz <shahbaazplatinum@gmail.com>
2022-03-11 18:08:53 +0530

    Discus Throw

---
+++ LongChallenges/Long-March(11-13)/DISCUS.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            int a[] = new int[3], temp = 0;
+            for (int i = 0; i < 3; i++) {
+                a[i] = sc.nextInt();
+                if (temp < a[i]) {
+                    temp = a[i];
+                }
+            }
+            System.out.println(temp);
+
+        }
+    }
+}
