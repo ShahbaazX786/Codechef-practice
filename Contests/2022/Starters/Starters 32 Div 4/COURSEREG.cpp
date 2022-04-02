4392e1fddd1a2986f85db6299106927624aa9db9
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-02 10:37:52 +0530

    Course Registration

---
+++ Contests/2022/Starters/Starters 32 Div 4/COURSEREG.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + c <= b)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
