3f4eb45bf44b193829f9169fe54cf7fffd670833
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-02 11:05:37 +0530

    Maximize the Minimum

---
+++ Contests/2022/Starters/Starters 32 Div 4/MAXTHEMIN.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        k = min(k, n - 1);
+        cout << a[k] << endl;
+    }
+    return 0;
+}
