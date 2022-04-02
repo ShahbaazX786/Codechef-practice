e7522c3a5bd67f20dc1ebd08a1ec1a06caf45576
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-02 10:36:52 +0530

    Volume Control

---
+++ Contests/2022/Starters/Starters 32 Div 4/VOLCONTROL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<abs((b-a))<<endl;
+	}
+	return 0;
+}
