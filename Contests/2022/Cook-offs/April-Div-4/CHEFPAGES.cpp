89898dfb10c0b6c8056b0fdbb17c10da84a882bf
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-04 09:52:06 +0530

    Important Pages on CodeChef

---
+++ Contests/2022/Cook-offs/April-Div-4/CHEFPAGES.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int a, b;
+    cin >> a >> b;
+    if (a == 1 and b == 1)
+    {
+        cout << "https://discuss.codechef.com" << endl;
+    }
+    else if (a)
+    {
+        cout << "https://www.codechef.com/contests" << endl;
+    }
+    else
+    {
+        cout << "https://www.codechef.com/practice" << endl;
+    }
+    return 0;
+}
